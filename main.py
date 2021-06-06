36a5289501acac3f868e4ba40980fc75c6123297
3jcn <31360292+3jcn@users.noreply.github.com>
2021-06-05 20:37:59 -0700

    Update main.py

--- main.py
+++ main.py
@@ -140,8 +140,8 @@ with header:
     st.markdown(html_temp,unsafe_allow_html=True)
     st.write('')
 
-sns.scatterplot(X.age, X.hemo, y, palette='Set1', legend=False)
-st.pyplot()
+fig,ax = sns.scatterplot(X.age, X.hemo, y, palette='Set1', legend=False)
+st.pyplot(fig)
 st.write('Confusion Matrix: (balanced dataset)', cm)
 st.write('')
 #button_r2 = st.button('R2 score of the model')

d7ff51a9972bac0745b8aa836284191a263d191b
3jcn <31360292+3jcn@users.noreply.github.com>
2023-10-13 11:46:28 -0700

    Update main.py

--- main.py
+++ main.py
@@ -84,12 +84,11 @@ with header:
     st.text('@Author: Thomas Nguyen Date: 28 Feb 2021')
     image = Image.open('ckd.png')
     st.image(image)
-    
-    st.text(":red[Big Brother messed up my data...All of my apps are not working properly.]")
 	
     st.text('CKD dataset from Kaggle: 400 x 25 features')
     df
-
+    st.text(':red[Big Brother messed up my data...All of my apps are not working properly.]')
+	
      # plot some graphs:
     st.subheader('Hypertension and Diabetes vs. CKD:')
     dfa = df[ df['htn']==0 ]

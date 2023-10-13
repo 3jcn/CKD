cab896a3f60135f8299a6088b2fcf5735b0809d3
3jcn <31360292+3jcn@users.noreply.github.com>
2023-10-13 11:40:18 -0700

    Update main.py

--- main.py
+++ main.py
@@ -85,8 +85,7 @@ with header:
     image = Image.open('ckd.png')
     st.image(image)
     
-    st.markdown('''
-    	:red[Big Brother messed up my data...All of my apps are not working properly.]''')
+    st.text(":red[Big Brother messed up my data...All of my apps are not working properly.]")
 	
     st.text('CKD dataset from Kaggle: 400 x 25 features')
     df
